4e4015b8f2c91d4f29499256a0f653df11c6ad5d
Francis Bilodeau <fbilodeau@dessinsdrummond.com>
2019-12-17 13:12:07 -0500

    Update AdsController.php

--- Controller/AdsController.php
+++ Controller/AdsController.php
@@ -27,32 +27,59 @@ class AdsController extends Controller
     public function txtAction()
     {
         $txtFile = $output = <<< RETURN
-appnexus.com, 2437, RESELLER, f5ab79cb980f11d1
+appnexus.com, 2437, DIRECT, f5ab79cb980f11d1
 
-google.com, pub-1847030151454431, RESELLER, f08c47fec0942fa0
-google.com, pub-9685734445476814, DIRECT, f08c47fec0942fa0
+google.com, pub-1847030151454431, DIRECT, f08c47fec0942fa0
 google.com, pub-6873606674919521, DIRECT, f08c47fec0942fa0
 google.com, pub-0037126603126973, DIRECT, f08c47fec0942fa0
-
+google.com, pub-9685734445476814, DIRECT, f08c47fec0942fa0
+google.com, pub-6283873300935465, DIRECT, f08c47fec0942fa0
 freeskreen.com, 178, DIRECT, fe119a6acfd19070
-slimcut.media, 178, DIRECT, fe119a6acfd19070
-indexexchange.com, 184088, RESELLER
+tremorhub.com, 030lb-bps80, DIRECT, 1a4e959a1b50034a
+indexexchange.com, 184088, RESELLER, 50b1c356f2c5c8fc
+quantum-advertising.com, 3247, RESELLER
+quantum-advertising.com, 3159, RESELLER
+quantum-advertising.com, 3198, RESELLER
+appnexus.com, 2579, RESELLER
+smartadserver.com, 1772, RESELLER
+advertising.com, 22118, RESELLER
 
-indexexchange.com, 184884, RESELLER, 50b1c356f2c5c8fc
-
-districtm.io, 101151, DIRECT
+districtm.io, 101151, DIRECT, 3fd707be9c4527c3
 appnexus.com, 1908, RESELLER, f5ab79cb980f11d1
 
-adtech.com, 10895, RESELLER
+adtech.com, 10895, DIRECT
+coxmt.com, 2000067907202, RESELLER
+pubmatic.com, 156078, RESELLER
+pubmatic.com, 156377, RESELLER
+pubmatic.com, 155967, RESELLER
+openx.com, 537143344, RESELLER
+indexexchange.com, 175407, RESELLER
+openx.com, 537125356, RESELLER
+
+lockerdome.com,11424682666850304,DIRECT
+
+appnexus.com, 3153, RESELLER, f5ab79cb980f11d1
+adtech.com, 11095, Reseller
+connectad.io, 73, DIRECT
+pubmatic.com, 156077, RESELLER, 5d62403b186f2ace
+adform.com, 768, RESELLER
+contextweb.com, 560489, RESELLER, 89ff185a4c4e857c
+
+openx.com, 540471645, DIRECT, 6a698e2ec38604c6
+
+pubmatic.com, 158605, DIRECT, 5d62403b186f2ace
+
+aps.amazon.com,5958391e-88ea-48ec-8c15-5013f0cdbc6d,DIRECT
+pubmatic.com,157150,RESELLER,5d62403b186f2ace
+openx.com,540191398,RESELLER,6a698e2ec38604c6
+appnexus.com,1908,RESELLER,f5ab79cb980f11d1
+yldbt.com,5b522cc167f6b300b89dc6d3,RESELLER,cd184cb30abaabb5
+adtech.com,12068,RESELLER
+districtm.io,100962,RESELLER,3fd707be9c4527c3
+rubiconproject.com,18020,RESELLER,0bfd66d529a55807
+rhythmone.com,1654642120,RESELLER,a670c89d4a324e47
 
 RETURN;
-
-        $userLocation = $this->get('ipLocation')->getUserLocation();
-        if (($userLocation->getIPRegion() != 'QUEBEC') && ($userLocation->getIPRegion() != 'ONTARIO')) {
-            $txtFile .= <<< RETURN
-google.com, pub-6283873300935465, DIRECT, f08c47fec0942fa0
-RETURN;
-        }
 
         // Dispatch request
         $response = new Response($txtFile);
