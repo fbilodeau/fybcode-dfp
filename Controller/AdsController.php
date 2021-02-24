a63f0034fe04fcc09f22de85b4abdf119b7693fe
Francis Bilodeau <fbilodeau@dessinsdrummond.com>
2021-02-24 11:05:44 -0500

    Update AdsController.php

--- Controller/AdsController.php
+++ Controller/AdsController.php
@@ -34,15 +34,13 @@ google.com, pub-6873606674919521, DIRECT, f08c47fec0942fa0
 google.com, pub-0037126603126973, DIRECT, f08c47fec0942fa0
 google.com, pub-9685734445476814, DIRECT, f08c47fec0942fa0
 google.com, pub-6283873300935465, DIRECT, f08c47fec0942fa0
-freeskreen.com, 178, DIRECT, fe119a6acfd19070
+
+telaria.com, 030lb-bps80, RESELLER, 1a4e959a1b50034a
 tremorhub.com, 030lb-bps80, RESELLER, 1a4e959a1b50034a
-indexexchange.com, 184088, RESELLER, 50b1c356f2c5c8fc
-quantum-advertising.com, 3247, RESELLER
-quantum-advertising.com, 3159, RESELLER
-quantum-advertising.com, 3198, RESELLER
-appnexus.com, 2579, RESELLER
-smartadserver.com, 1772, RESELLER
+freeskreen.com, 178, DIRECT, fe119a6acfd19070
 advertising.com, 22118, RESELLER
+smartadserver.com, 1772, RESELLER
+indexexchange.com, 184088, RESELLER, 50b1c356f2c5c8fc
 
 districtm.io, 101151, DIRECT, 3fd707be9c4527c3
 appnexus.com, 1908, RESELLER, f5ab79cb980f11d1
@@ -59,23 +57,21 @@ openx.com, 540471645, DIRECT, 6a698e2ec38604c6
 
 pubmatic.com, 158605, DIRECT, 5d62403b186f2ace
 
-aps.amazon.com,5958391e-88ea-48ec-8c15-5013f0cdbc6d,DIRECT
-pubmatic.com,157150,RESELLER,5d62403b186f2ace
-openx.com,540191398,RESELLER,6a698e2ec38604c6
-appnexus.com,1908,RESELLER,f5ab79cb980f11d1
-yldbt.com,5b522cc167f6b300b89dc6d3,RESELLER,cd184cb30abaabb5
-adtech.com,12068,RESELLER
-districtm.io,100962,RESELLER,3fd707be9c4527c3
-rubiconproject.com,18020,RESELLER,0bfd66d529a55807
-rhythmone.com,1654642120,RESELLER,a670c89d4a324e47
 
-ad-generation.jp,12474,RESELLER
+aps.amazon.com,5958391e-88ea-48ec-8c15-5013f0cdbc6d,DIRECT
+ad-generation.jp,12474,RESELLER,7f4ea9029ac04e53
 appnexus.com,3663,RESELLER,f5ab79cb980f11d1
+districtm.io,100962,RESELLER,3fd707be9c4527c3
+gumgum.com,14141,RESELLER,ffdef49475d318a9
+openx.com,540191398,RESELLER,6a698e2ec38604c6
+pubmatic.com,157150,RESELLER,5d62403b186f2ace
+pubmatic.com,160006,RESELLER,5d62403b186f2ace
+pubmatic.com,160096,RESELLER,5d62403b186f2ace
+rhythmone.com,1654642120,RESELLER,a670c89d4a324e47
+rubiconproject.com,18020,RESELLER,0bfd66d529a55807
+smaato.com,1100044650,RESELLER,07bcf65f187117b4
 yahoo.com,55029,RESELLER,e1a5b5b6e3255540
 
-indexexchange.com, 192410, Reseller, 50b1c356f2c5c8fc
-152media.com, 152, DIRECT
-appnexus.com, 3153, RESELLER, f5ab79cb980f11d1
 
 m32.media, 1033, DIRECT
 m32connect.com, 1033, DIRECT
@@ -115,7 +111,6 @@ freewheel.tv, 1131121, RESELLER
 freewheel.tv, 1131329, RESELLER
 
 indexexchange.com, 184884, DIRECT, 50b1c356f2c5c8fc
-telaria.com, 030lb-bps80, RESELLER, 1a4e959a1b50034a
 yahoo.com, 56566, DIRECT, e1a5b5b6e3255540
 
 RETURN;
